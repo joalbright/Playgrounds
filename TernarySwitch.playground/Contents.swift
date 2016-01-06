f3adf667d714b5080b44355108565153d1e0e7bd
Jo Albright <me@jo2.co>
2016-01-06 17:31:10 -0500

    ternary switch exploration

--- TernarySwitch.playground/Contents.swift
+++ TernarySwitch.playground/Contents.swift
@@ -5,8 +5,8 @@ import UIKit
 // ??? loops array and returns first element where lhs == element.0
 infix operator ??? { associativity left precedence 200 }
 
-// || merges values into an array
-infix operator || { associativity left precedence 210 }
+// ||| merges values into an array
+infix operator ||| { associativity left precedence 210 }
 
 // --> convert lhs & rhs to tuple (lhs,rhs)
 infix operator --> { associativity left precedence 220 }
@@ -23,31 +23,31 @@ func ??? <T: Equatable>(lhs: T, rhs: [(T,AnyObject)]) -> AnyObject? {
 
 }
 
-func || <T>(lhs: (T,AnyObject), rhs: (T,AnyObject)) -> [(T,AnyObject)] {
+func ||| <T>(lhs: (T,AnyObject), rhs: (T,AnyObject)) -> [(T,AnyObject)] {
     
     return [lhs,rhs]
 
 }
 
-func || <T>(lhs: [(T,AnyObject)], rhs: (T,AnyObject)) -> [(T,AnyObject)] {
+func ||| <T>(lhs: [(T,AnyObject)], rhs: (T,AnyObject)) -> [(T,AnyObject)] {
     
     return lhs + [rhs]
 
 }
 
-func || <T>(lhs: (Range<T>,AnyObject), rhs: (Range<T>,AnyObject)) -> [(T,AnyObject)] {
+func ||| <T>(lhs: (Range<T>,AnyObject), rhs: (Range<T>,AnyObject)) -> [(T,AnyObject)] {
     
     return lhs.0.map { ($0,lhs.1) } + rhs.0.map { ($0,rhs.1) }
     
 }
 
-func || <T>(lhs: (Range<T>,AnyObject), rhs: (T,AnyObject)) -> [(T,AnyObject)] {
+func ||| <T>(lhs: (Range<T>,AnyObject), rhs: (T,AnyObject)) -> [(T,AnyObject)] {
     
     return lhs.0.map { ($0,lhs.1) } + [rhs]
 
 }
 
-func || <T>(lhs: [(T,AnyObject)], rhs: (Range<T>,AnyObject)) -> [(T,AnyObject)] {
+func ||| <T>(lhs: [(T,AnyObject)], rhs: (Range<T>,AnyObject)) -> [(T,AnyObject)] {
     
     return lhs + rhs.0.map { ($0,rhs.1) }
 
@@ -59,6 +59,12 @@ func --> <T>(lhs: T, rhs: AnyObject) -> (T,AnyObject) {
 
 }
 
+func ??? <T: Equatable>(lhs:T?, rhs: (T,AnyObject)...) -> AnyObject? {
+    
+    for r in rhs { if lhs == r.0 { return r.1 } }; return nil
+    
+}
+
 enum LifeStatus { case Alive, Dead, Zombie }
 
 let life: LifeStatus? = .Dead
@@ -67,12 +73,24 @@ let life: LifeStatus? = .Dead
 let color1 = life == .Alive ? UIColor.greenColor() : life == .Dead ? UIColor.redColor() : life == .Zombie ? UIColor.grayColor() : UIColor.whiteColor()
 
 // using custom operators
-let color2 = life ??? .Alive --> UIColor.greenColor() || .Dead --> UIColor.redColor() || .Zombie --> UIColor.grayColor() ?? UIColor.whiteColor()
+let color2 = life ??? .Alive --> UIColor.greenColor() ||| .Dead --> UIColor.redColor() ||| .Zombie --> UIColor.grayColor() ?? UIColor.whiteColor()
+
+func text<T>(a: (T,AnyObject)...) -> [(T,AnyObject)] {
+    
+    return a
+    
+}
+
+life ??? [LifeStatus.Alive --> UIColor.greenColor(), LifeStatus.Dead --> UIColor.redColor(), LifeStatus.Zombie --> UIColor.grayColor()]
+
+
+let color3 = life ??? [.Alive --> UIColor.greenColor(), .Dead --> UIColor.redColor(), .Zombie --> UIColor.grayColor()] ?? UIColor.whiteColor()
+
 
 let age = 15
 
 // works with ranges
-//let ageGroup = age ??? (0...1) --> "baby" || (2...4) --> "toddler" || (5...12) --> "kid" || (13...19) --> "teen" ?? "adult"
+//let ageGroup = age ??? (0...1) --> "baby" ||| (2...4) --> "toddler" ||| (5...12) --> "kid" ||| (13...19) --> "teen" ?? "adult"
 //
 //ageGroup
 
