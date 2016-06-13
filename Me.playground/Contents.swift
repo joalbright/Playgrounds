5d7c033768e1bf7e1f91d163bf7f4807384abb78
Jo Albright <me@jo2.co>
2016-06-13 16:42:19 -0400

    ummm... dunno

---
+++ Me.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Designer { }
+class Developer { }
+class Nerd { }
+
+
+class Me : Designer, Developer, Nerd { }
+
+let me = Me()
+me.name = “Jo”
---
+++ Random.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+
+view.backgroundColor = .redColor()
+
+view
+
+
